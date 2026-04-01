1dd0524c6023399c2226b7f010614427502d6262
Nitin Sable <sablenitin1432@gmail.com>
2026-04-01 17:23:07 +0530

    fixed backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
 
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://food-backend-4bi7.onrender.com";
     const [token, setToken] = useState("");
     const [food_list, setFOODList] = useState([])
 
